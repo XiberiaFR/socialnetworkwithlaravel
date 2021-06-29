6f34496bb7f29a9e99944dea96057fccf892b395
XiberiaFR <62890250+XiberiaFR@users.noreply.github.com>
2021-06-29 23:47:59 +0200

    affichage des users sur la home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="panel-body" id="list-1" v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="10">
-   <example-component :message="message"></example-component>
+<div class="card">
+    <example-component :users="users"></example-component>
 </div>
 @endsection
--- storage/framework/views/af88216f4435459f64e460113d05909cbbb66a38.php
+++ storage/framework/views/af88216f4435459f64e460113d05909cbbb66a38.php
@@ -1,6 +1,6 @@
 <?php $__env->startSection('content'); ?>
-<div class="panel-body" id="list-1" v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="10">
-   <example-component :message="message"></example-component>
+<div class="card">
+    <example-component :users="users"></example-component>
 </div>
 <?php $__env->stopSection(); ?>
 
